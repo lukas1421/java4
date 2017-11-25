b9bdc49fdcd02f6425953de8016a348e7b6e89a0
lukas1421 <shijiluke@gmail.com>
2017-11-25 23:35:37 +0800

    1125 pm

--- Trading/apidemo/MorningTask.java
+++ Trading/apidemo/MorningTask.java
@@ -546,7 +546,7 @@ public final class MorningTask implements HistoricalHandler {
                     Utility.simpleWrite("US CLOSE" + "\t" + close + "\t" + ldt.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"))
                             + "\t" + zdt.getHour(), true);
                     Utility.simpleWriteToFile("SGXA50" + "\t" + close, false, fxOutput);
-                    Utility.simpleWriteToFile("SGXA50BM" + "\t" + close, false, fxOutput);
+                    Utility.simpleWriteToFile("SGXA50BM" + "\t" + close, true, fxOutput);
                     break;
             }
         }
