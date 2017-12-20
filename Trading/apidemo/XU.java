ef9baaa85826a8f462b4b165a43a83350039ae54
lukas1421 <shijiluke@gmail.com>
2017-12-20 15:06:20 +0800

    1220 after market

--- Trading/apidemo/XU.java
+++ Trading/apidemo/XU.java
@@ -96,7 +96,9 @@ public final class XU extends JPanel {
     XU() {
         LocalTime lt = LocalTime.of(8, 45);
         while (lt.isBefore(LocalTime.of(16, 1))) {
-            tradeTimeXU.add(lt);
+            if(!(lt.isAfter(LocalTime.of(11,30)) && lt.isBefore(LocalTime.of(13,0)))) {
+                tradeTimeXU.add(lt);
+            }
             lt = lt.plusMinutes(1L);
         }
 
